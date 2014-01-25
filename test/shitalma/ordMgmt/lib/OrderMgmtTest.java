7926253756bf4311a1af095e9829595874a4da9f
Shital <shitalma@thoughtworks.com>
2014-01-25 12:04:52 +0530

    Assertion done for create table

--- test/shitalma/ordMgmt/lib/OrderMgmtTest.java
+++ test/shitalma/ordMgmt/lib/OrderMgmtTest.java
@@ -26,14 +26,14 @@ public class OrderMgmtTest {
             conn = DriverManager.getConnection(DB_URL, USER, PASS);
             stmt = conn.createStatement();
             String sql = "CREATE SCHEMA ordMgmt";
-            stmt.executeUpdate(sql);
+            assertEquals(1, stmt.executeUpdate(sql));
 
             String createProductTable = "CREATE TABLE ordMgmt.product (\n" +
                     "\tprod_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
                     "\tprod_name VARCHAR(30),\n" +
                     "\tunit_price FLOAT\n" +
                     ");";
-            stmt.executeUpdate(createProductTable);
+            assertEquals(0, stmt.executeUpdate(createProductTable));
 
             String createCustomerTable = "CREATE TABLE ordMgmt.customer (\n" +
                     "\tcust_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
@@ -43,7 +43,7 @@ public class OrderMgmtTest {
                     "\tstate VARCHAR(30),\n" +
                     "\tcontact BIGINT\n" +
                     ")";
-            stmt.executeUpdate(createCustomerTable);
+            assertEquals(0, stmt.executeUpdate(createCustomerTable));
 
             String createOrderInfoTable = "CREATE TABLE ordMgmt.orderInfo (\n" +
                     "\torder_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
@@ -51,7 +51,8 @@ public class OrderMgmtTest {
                     "\tdate_of_order DATETIME,\n" +
                     "\tdelivery_date DATETIME \n" +
                     ")";
-            stmt.executeUpdate(createOrderInfoTable);
+            assertEquals(0, stmt.executeUpdate(createOrderInfoTable));
+
 
             String createOrderItemTable = "CREATE TABLE ordMgmt.orderItems (\n" +
                     "\torder_item_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
@@ -60,22 +61,23 @@ public class OrderMgmtTest {
                     "\tquantity INT,\n" +
                     "\titem_price FLOAT\n" +
                     ")";
-            stmt.executeUpdate(createOrderItemTable);
+            assertEquals(0, stmt.executeUpdate(createOrderItemTable));
+
 
             String addForeignKeyToOrderInfo = "ALTER TABLE ordMgmt.orderInfo \n" +
                     "\tADD CONSTRAINT orderInfo_custID_fk FOREIGN KEY(cust_id)\n" +
                     "\tREFERENCES customer(cust_id);\n";
-            stmt.executeUpdate(addForeignKeyToOrderInfo);
+            assertEquals(0, stmt.executeUpdate(addForeignKeyToOrderInfo));
 
             String addForeignKeyToOrderItems = "ALTER TABLE ordMgmt.orderItems \n" +
                     "\tADD CONSTRAINT orderIems_orderId_fk FOREIGN KEY(order_id)\n" +
                     "\tREFERENCES orderInfo(order_id)\n";
-            stmt.executeUpdate(addForeignKeyToOrderItems);
+            assertEquals(0, stmt.executeUpdate(addForeignKeyToOrderItems));
 
             String addForeignKeyToOrderItemsForProduct = "ALTER TABLE ordMgmt.orderItems \n" +
                     "\tADD CONSTRAINT orderIems_prodId_fk FOREIGN KEY(prod_id)\n" +
                     "\tREFERENCES product(prod_id);\n";
-            stmt.executeUpdate(addForeignKeyToOrderItemsForProduct);
+            assertEquals(0, stmt.executeUpdate(addForeignKeyToOrderItemsForProduct));
 
         } catch(SQLException se){
             se.printStackTrace();
