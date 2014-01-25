7cff8b6c072ba08a46a0596ed35a204c54fe2ea7
Shital <shitalma@thoughtworks.com>
2014-01-25 15:04:25 +0530

    test case is done for delete functionality

--- test/shitalma/ordMgmt/lib/OrderMgmtTest.java
+++ test/shitalma/ordMgmt/lib/OrderMgmtTest.java
@@ -28,7 +28,7 @@ public class OrderMgmtTest {
         String sql = "CREATE SCHEMA ordMgmt";
         assertEquals(1, stmt.executeUpdate(sql));
 
-        try{
+        try {
 
             String createProductTable = "CREATE TABLE ordMgmt.product (\n" +
                     "\tprod_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
@@ -81,87 +81,99 @@ public class OrderMgmtTest {
                     "\tREFERENCES product(prod_id);\n";
             assertEquals(0, stmt.executeUpdate(addForeignKeyToOrderItemsForProduct));
 
-        } catch(SQLException se){
+        } catch (SQLException se) {
             se.printStackTrace();
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
     @Test
-    public void testInsertRecordsIntoProductTable() throws Exception{
+    public void testInsertSingleRecordIntoProductTable() throws Exception {
         String firstRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('Pen',10)";
-        assertEquals(1 , stmt.executeUpdate(firstRecord));
+        assertEquals(1, stmt.executeUpdate(firstRecord));
 
-        String secondRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('NotebooK',30)";
-        assertEquals(1 , stmt.executeUpdate(secondRecord));
-
-        String thirdRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('Pencil',3)";
-        assertEquals(1 , stmt.executeUpdate(thirdRecord));
-
-        String forthRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('Water Colors',25)";
-        assertEquals(1 , stmt.executeUpdate(forthRecord));
     }
 
     @Test
-    public void testForSelectingRecordsFromProductTable() throws Exception{
+    public void testForInsertMultipleRecordsIntoProductTable() throws Exception {
+        String secondRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('NotebooK',30)";
+        assertEquals(1, stmt.executeUpdate(secondRecord));
+
+        String thirdRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('Pencil',3)";
+        assertEquals(1, stmt.executeUpdate(thirdRecord));
+
+        String forthRecord = "INSERT INTO ordMgmt.product(prod_name,unit_price) VALUES('Water Colors',25)";
+        assertEquals(1, stmt.executeUpdate(forthRecord));
+    }
+
+    @Test
+    public void testForSelectingRecordsFromProductTable() throws Exception {
         String sql = "SELECT * from ordMgmt.product;";
         ResultSet rs = stmt.executeQuery(sql);
 
-        String[] productName = {"Pen","NotebooK","Pencil","Water Colors"};
-        int[] unitPrice = {10,30,3,25};
+        String[] productName = {"Pen", "NotebooK", "Pencil", "Water Colors"};
+        int[] unitPrice = {10, 30, 3, 25};
 
         while (rs.next()) {
-            assertEquals(productName[rs.getRow()-1] , rs.getString(2));
-            assertEquals(unitPrice[rs.getRow()-1] , rs.getInt(3));
+            assertEquals(productName[rs.getRow() - 1], rs.getString(2));
+            assertEquals(unitPrice[rs.getRow() - 1], rs.getInt(3));
         }
     }
+
     @Test
-    public void testForInsertRecordIntoCustomerTable () throws  Exception {
+    public void testForInsertRecordIntoCustomerTable() throws Exception {
         String sql1 = "INSERT INTO ordMgmt.customer(cust_name ,address ,city ,state ,contact ) VALUES('Shital','3rd block','Kormangala','Karnataka',8123852388);";
-        assertEquals(1 , stmt.executeUpdate(sql1));
+        assertEquals(1, stmt.executeUpdate(sql1));
     }
 
     @Test
-    public void testForInsertingRecordIntoOrderInfoTable() throws Exception{
+    public void testForInsertingRecordIntoOrderInfoTable() throws Exception {
 
         String inOrderInfo = "INSERT INTO ordMgmt.orderInfo(cust_id,date_of_order,delivery_date) VALUES(1,now(),now())";
-        assertEquals(1 , stmt.executeUpdate(inOrderInfo));
+        assertEquals(1, stmt.executeUpdate(inOrderInfo));
 
     }
 
     @Test
     public void testForInsertingRecordIntoOrderItemsUsingSelectSubquery() throws Exception {
         String inOrderItem = "INSERT INTO ordMgmt.orderItems(order_id,prod_id,quantity,item_price)VALUES((SELECT MAX(order_id) from ordMgmt.orderInfo where cust_id =1),(SELECT prod_id from ordMgmt.product where prod_name='Pen'),10,(SELECT 10*unit_price from ordMgmt.product where prod_name='Pen'))";
-        assertEquals(1,stmt.executeUpdate(inOrderItem));
+        assertEquals(1, stmt.executeUpdate(inOrderItem));
     }
 
     @Test
     public void testForGeneratingPaymentUsingSelectQuery() throws Exception {
-       ResultSet rs;
-       String gettingPayment = "SELECT ordMgmt.orderItems.order_id,ordMgmt.customer.cust_name,ordMgmt.orderItems.item_price "+
-                "FROM ordMgmt.orderItems INNER JOIN ordMgmt.orderInfo INNER JOIN ordMgmt.customer "+
+        ResultSet rs;
+        String gettingPayment = "SELECT ordMgmt.orderItems.order_id,ordMgmt.customer.cust_name,ordMgmt.orderItems.item_price " +
+                "FROM ordMgmt.orderItems INNER JOIN ordMgmt.orderInfo INNER JOIN ordMgmt.customer " +
                 "ON ordMgmt.orderItems.order_id = ordMgmt.orderInfo.order_id AND ordMgmt.orderInfo.cust_id = ordMgmt.customer.cust_id WHERE ordMgmt.orderItems.order_id IN (SELECT MAX(order_id) from ordMgmt.orderItems)";
         rs = stmt.executeQuery(gettingPayment);
 
         while (rs.next()) {
-            assertEquals(1 , rs.getInt(1));
-            assertEquals("Shital",(rs.getString(2)));
-            assertEquals(100,rs.getInt(3));
+            assertEquals(1, rs.getInt(1));
+            assertEquals("Shital", (rs.getString(2)));
+            assertEquals(100, rs.getInt(3));
         }
+    }
+
+    @Test
+    public void testForDeleteRecordFromProductTable() throws Exception {
+        String firstRecord = "DELETE FROM ordMgmt.product WHERE prod_name ='Pen'";
+        ResultSet rs = stmt.executeQuery(firstRecord);
+        assertEquals(0, rs.getRow());
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
-        try
-        {
+        try {
             String dropQuery = "DROP SCHEMA ordMgmt";
             stmt.executeUpdate(dropQuery);
             stmt.close();
             conn.close();
-        } catch(SQLException se){
+        } catch (SQLException se) {
             se.printStackTrace();
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
